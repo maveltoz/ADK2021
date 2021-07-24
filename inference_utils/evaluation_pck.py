878eb9e28c09025d261b77c9f9401ab6bb8d6701
Dae-Hoon, KIM <eogns0707@gmail.com>
2021-07-25 02:43:00 +0900

    Update evaluation_pck.py

--- inference_utils/evaluation_pck.py
+++ inference_utils/evaluation_pck.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import json
 import xml.etree.ElementTree as ET
 from mmpose.core import keypoint_pck_accuracy
 
@@ -20,6 +21,50 @@ def get_single_pck(result):
             keypoints = res['keypoints']
 
     return keypoints[:, :-1]
+
+
+def get_pck_json(preds, ann_root='data/test/challenge_annotations/'):
+    preds = np.array(preds)
+    gts = []
+    masks = []
+    thr = 0.2
+    normalize = []
+
+    entries = os.listdir(ann_root)
+
+    for entry in entries:
+        with open(ann_root + entry) as annotation_file:
+            annotation = json.load(annotation_file)['label_info']
+
+            w = int(annotation['image']['width'])
+            h = int(annotation['image']['height'])
+            bbox_thr = np.max([w, h])
+            normalize.append([bbox_thr, bbox_thr])
+
+            now_gt = []
+            now_mask = []
+
+            for k in range(17):
+                x = annotation['annotations'][0]['keypoints'][3 * k]
+                y = annotation['annotations'][0]['keypoints'][3 * k + 1]
+                z = annotation['annotations'][0]['keypoints'][3 * k + 2]
+                gt = [x, y]
+                now_gt.append(gt)
+                if z == 2:
+                    mask = True
+                else:
+                    mask = False
+                now_mask.append(mask)
+            gts.append(now_gt)
+            masks.append(now_mask)
+
+    gts = np.array(gts)
+    masks = np.array(masks)
+    normalize = np.array(normalize)
+
+    pck = keypoint_pck_accuracy(preds, gts, masks, thr, normalize)
+
+    return pck
 
 
 def get_pck(preds, ann_root='data/test/annotation/'):
