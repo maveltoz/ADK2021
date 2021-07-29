996ba5362ef0e527b14749ba64759810777db0da
Dae-Hoon, KIM <eogns0707@gmail.com>
2021-07-29 15:34:34 +0900

    Update __init__.py

--- keypoint_detector/mmpose/apis/__init__.py
+++ keypoint_detector/mmpose/apis/__init__.py
@@ -1,5 +1,4 @@
-from .inference import (inference_bottom_up_pose_model,
-                        inference_top_down_pose_model, init_pose_model,
+from .inference import (inference_top_down_pose_model, init_pose_model,
                         vis_pose_result)
 from .inference_3d import (extract_pose_sequence, inference_interhand_3d_model,
                            inference_mesh_model, inference_pose_lifter_model,
@@ -9,8 +8,7 @@ from .test import multi_gpu_test, single_gpu_test
 from .train import train_model
 
 __all__ = [
-    'train_model', 'init_pose_model', 'inference_top_down_pose_model',
-    'inference_bottom_up_pose_model', 'multi_gpu_test', 'single_gpu_test',
+    'train_model', 'init_pose_model', 'inference_top_down_pose_model', 'multi_gpu_test', 'single_gpu_test',
     'vis_pose_result', 'get_track_id', 'vis_pose_tracking_result',
     'inference_pose_lifter_model', 'vis_3d_pose_result',
     'inference_interhand_3d_model', 'extract_pose_sequence',
