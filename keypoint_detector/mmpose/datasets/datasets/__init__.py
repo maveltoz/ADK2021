0618e17d8cd1d35ca8801717db10250c3126c527
Dae-Hoon, KIM <eogns0707@gmail.com>
2021-07-30 21:27:43 +0900

    Update __init__.py

--- keypoint_detector/mmpose/datasets/datasets/__init__.py
+++ keypoint_detector/mmpose/datasets/datasets/__init__.py
@@ -1,6 +1,7 @@
 from .animal import AnimalPoseDataset
+from .animal import CustomDataset
 from .top_down import TopDownCocoDataset
 
 __all__ = [
-    'TopDownCocoDataset','AnimalPoseDataset'
+    'TopDownCocoDataset','AnimalPoseDataset', 'CustomDataset'
 ]
