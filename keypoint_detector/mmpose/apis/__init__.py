5b8492b38ead96838cb6e0e86c860da1f61c8d82
Dae-Hoon, KIM <eogns0707@gmail.com>
2021-08-04 15:24:39 +0900

    Update __init__.py

--- keypoint_detector/mmpose/apis/__init__.py
+++ keypoint_detector/mmpose/apis/__init__.py
@@ -1,9 +1,6 @@
-from .inference import (inference_top_down_pose_model, init_pose_model,
-                        vis_pose_result)
 from .test import multi_gpu_test, single_gpu_test
 from .train import train_model
 
 __all__ = [
-    'train_model', 'init_pose_model', 'inference_top_down_pose_model', 'multi_gpu_test', 'single_gpu_test',
-    'vis_pose_result'
+    'train_model', 'multi_gpu_test', 'single_gpu_test'
 ]
