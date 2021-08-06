13a44fdbd652f98c29fb865926b6ddd37585e769
Dae-Hoon, KIM <eogns0707@gmail.com>
2021-08-06 11:52:32 +0900

    Update hrnet.py

--- configs/hrnet.py
+++ configs/hrnet.py
@@ -79,6 +79,7 @@ model = dict(
     train_cfg=dict(),
     test_cfg=dict(
         flip_test=True,
+#         post_process='unbiased',
         post_process='default',
         shift_heatmap=True,
         modulate_kernel=11))
